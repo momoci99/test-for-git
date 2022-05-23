a785e35729b199ecc5d395c92835d8793688cbb1
ykmo-VIRNECT <62318056+ykmo-VIRNECT@users.noreply.github.com>
2022-05-23 23:56:16 +0900

    update:more mainmain.js

--- mainmain.js
+++ mainmain.js
@@ -4,4 +4,12 @@ setTimeout(() => {
     console.log('RrRRRR')
     console.log('RrRRRR')
     console.log('RrRRRR')
+    console.log('RrRRRR')
+    console.log('RrRRRR')
+    console.log('RrRRRR')
+    console.log('RrRRRR')
+    console.log('RrRRRR')
+    console.log('RrRRRR')
+    console.log('RrRRRR')
+    console.log('RrRRRR')
 }, (1000));
